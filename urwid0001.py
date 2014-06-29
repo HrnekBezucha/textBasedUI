08ec5e56c052e1ca2ec32ecdf0f5ffeb2bbca0e9
HakitoCZ <hakito.cz@gmail.com>
2014-06-29 17:27:39 +0100

    prints pressed button

---
+++ urwid0001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+App prints pressed button
+'''
+import urwid
+
+def show_or_exit(key):
+  if key in ('q', 'Q'):
+    raise urwid.ExitMainLoop()
+  txt.set_text(repr(key))
+
+txt = urwid.Text(u"Press any key or 'Q' for quit.")
+fill = urwid.Filler(txt, 'top')
+loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+loop.run()
+
