0e1f6922b75d30b5be29382ab9ae9fbf32bd3dcc
HakitoCZ <hakito.cz@gmail.com>
2014-06-29 18:24:13 +0100

    fancy gradient banner

---
+++ urwid0003.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Description: Prints fancy banner
+Source: http://urwid.org/tutorial/index.html#high-color-modes
+'''
+import urwid
+
+def exit_on_q(key):
+  if key in ('q', 'Q'):
+    raise urwid.ExitMainLoop()
+
+palette = [
+  ('banner', '', '', '', '#ffa', '#60d'),
+  ('streak', '', '', '', 'g50', '#60a'),
+  ('inside', '', '', '', 'g38', '#808'),
+  ('outside', '', '', '', 'g27', '#a06'),
+  ('bg', '', '', '', 'g7', '#d06'),]
+
+placeholder = urwid.SolidFill()
+loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
+loop.screen.set_terminal_properties(colors=256)
+loop.widget = urwid.AttrMap(placeholder, 'bg')
+loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
+
+div = urwid.Divider()
+outside = urwid.AttrMap(div, 'outside')
+inside = urwid.AttrMap(div, 'inside')
+txt = urwid.Text(('banner', u" Fancier banner \n ('Q' for quit) "), 
+  align='center')
+streak = urwid.AttrMap(txt, 'streak')
+pile = loop.widget.base_widget # base widget skips decorations
+for item in [outside, inside, streak, inside, outside]:
+  pile.contents.append((item, pile.options()))
+
+loop.run()
+
